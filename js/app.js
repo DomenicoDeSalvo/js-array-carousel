b47b12c005944aee1d1124e4347f97bfd891dc23
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-03-10 17:00:41 +0100

    Added cicle to js

---
+++ js/app.js
@@ -0,0 +1,30 @@
+//Codice per carosello.
+
+//Array contenente i percorsi alle immagini da utilizzare.
+
+const pathsArray = [
+    "./img/01.webp",
+    "./img/02.webp",
+    "./img/03.webp",
+    "./img/04.webp",
+    "./img/05.webp"
+]; //Array
+
+//Acquisizione elemento "album".
+const sliderElement = document.querySelector('.album'); //Element | Null
+
+//Creazione ciclo per assegnare le immagini.
+for(let i = 0; i = pathsArray < 1; i++) {
+
+    const imagePath = pathsArray[i]; //String
+    //Creazione variabile a cui si assegna la stringa con annessa immagine.
+    const albumItem = `
+    <div class="pic">
+        <img src="${imagePath}">
+    </div> 
+    `; //Element
+
+    //Inclusione della variabile nel codice
+
+
+}
